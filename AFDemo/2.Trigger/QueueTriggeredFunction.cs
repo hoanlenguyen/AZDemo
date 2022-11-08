42e0c395b5ecce04693a2f7c625657f44c44c9a7
hoan.le <hoan.le@saigontechnology.com>
2022-11-08 11:53:24 +0700

    update code

--- AFDemo/2.Trigger/QueueTriggeredFunction.cs
+++ AFDemo/2.Trigger/QueueTriggeredFunction.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
 
 namespace AFDemo.Trigger
 {
@@ -10,5 +7,11 @@ namespace AFDemo.Trigger
     {
         //https://medium.com/swlh/how-to-use-azure-queue-triggered-functions-and-why-7f651c9d3f8c
         //https://www.programmingwithwolfgang.com/azure-functions-process-queue-messages/
+        //https://stackoverflow.com/questions/13087872/test-azure-service-bus-locally-without-any-subscription-or-login
+        [FunctionName("OrderNameUpdateFunction")]
+        public static void Run([ServiceBusTrigger("CustomerQueue", Connection = "YourConnectionString")] string myQueueItem, ILogger log)
+        {
+            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+        }
     }
 }
---
+++ AFDemo/Data/MyDbContext.cs
@@ -0,0 +1,31 @@
+﻿using AFDemo.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace AFDemo.Data
+{
+    public class MyDbContext : DbContext
+    {
+        private readonly IConfiguration configuration;
+
+        public MyDbContext(DbContextOptions<MyDbContext> options, IConfiguration configuration)
+            : base(options)
+        {
+            this.configuration = configuration;
+        }
+
+        public virtual DbSet<Order> Orders { get; set; }
+
+        protected override async void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
+            SQLitePCL.Batteries.Init();
+            await Database.MigrateAsync();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
