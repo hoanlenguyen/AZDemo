69be3ac2adea9c501dd2f0f60b4d06a7a0ca2d62
hoan.le <hoan.le@saigontechnology.com>
2022-11-11 16:35:51 +0700

    sync

--- AFDemo/2.Trigger/QueueTriggeredFunction.cs
+++ AFDemo/2.Trigger/QueueTriggeredFunction.cs
@@ -5,9 +5,6 @@
 //{
 //    public class QueueTriggeredFunction
 //    {
-//        //https://medium.com/swlh/how-to-use-azure-queue-triggered-functions-and-why-7f651c9d3f8c
-//        //https://www.programmingwithwolfgang.com/azure-functions-process-queue-messages/
-//        //https://stackoverflow.com/questions/13087872/test-azure-service-bus-locally-without-any-subscription-or-login
 //        //[FunctionName("OrderNameUpdateFunction")]
 //        //public static void Run([ServiceBusTrigger("CustomerQueue", Connection = "YourConnectionString")] string myQueueItem, ILogger log)
 //        //{
